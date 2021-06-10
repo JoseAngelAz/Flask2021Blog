da4fd720bc256988ec8d6ba34844a2e2bebb757e
JoseAngelAz <angelvanitas555@gmail.com>
2021-06-10 02:01:59 -0600

    hay que probar las querys

--- flaskblog.py
+++ flaskblog.py
@@ -29,7 +29,7 @@ class Post(db.Model):
     title = db.Column(db.String(20), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default = datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.Foreingkey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     def __repr__(self):
         return f"User('{self.title}','{self.date_posted}')"
 #posts
